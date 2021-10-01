056ef8749be20adcaa57292fc0265cc934d3bdc2
Frederico Gama <91661291+fae59@users.noreply.github.com>
2021-10-01 20:41:55 -0300

    Update MinAvgTwoSlice.c

--- MinAvgTwoSlice.c
+++ MinAvgTwoSlice.c
@@ -1,19 +1,19 @@
 int solution(int A[], int N) {
     // write your code in C99 (gcc 6.2.0)
-    double min_idx = 0;
-    double min_value = 100001;
+    double minI = 0;
+    double minVal = 100001;
 
-    for (int idx=0;idx<N;idx++) {
-        if (((A[idx] + A[idx+1]) / 2.0) < min_value) {
-            min_idx = idx;
-            min_value = (A[idx] + A[idx+1]) / 2.0;
+    for (int i=0;i<N;i++) {
+        if (((A[i] + A[i+1]) / 2.0) < minVal) {
+            minI = i;
+            minVal = (A[i] + A[i+1]) / 2.0;
         }
 
-        if(idx<(N-2) && (((A[idx] + A[idx+1] + A[idx+2]) / 3.0) < min_value)) {
-            min_idx = idx;
-            min_value = (A[idx] + A[idx+1] + A[idx+2]) / 3.0;
+        if(idx<(N-2) && (((A[i] + A[i+1] + A[i+2]) / 3.0) < minVal)) {
+            minI = i;
+            minVal = (A[i] + A[i+1] + A[i+2]) / 3.0;
         }
     }
     
-    return min_idx;
+    return minI;
 }
