4b7977c06a2795212504e730235d8d6df5c96f5f
Frederico Gama <91661291+fae59@users.noreply.github.com>
2021-10-13 14:24:33 -0300

    Update testeSidia2.cpp

--- testeSidia2.cpp
+++ testeSidia2.cpp
@@ -3,7 +3,7 @@
 #include <bits/stdc++.h>
 int solution(vector<int> &A) {
     // write your code in C++14 (g++ 6.2.0)
-    const string S = {"00:01:07,400-234-090\n00:05:01,701-080-080\n00:05:00,400-234-090"};
+    //const string S = {"00:01:07,400-234-090\n00:05:01,701-080-080\n00:05:00,400-234-090"};
     const int N = S.size();
     const int num_calls = floor(N/20);
     int k = 0;
@@ -15,9 +15,9 @@ int solution(vector<int> &A) {
 
     for (long i=0;i<N;i++) {
         if (S[i+2] == ':' && S[i+5] == ':' && S[i+9] != '0') {
-            calls_dur_hor[k] = (S[i]-‘0’)*10 + (S[i+1]-‘0’);
-            calls_dur_min[k] = (calls_dur_hor[k] * 60) + ((S[I+3]-‘0’)*10 + S[i+4]-'0'); 
-            calls_dur_seg[k] = (S[i+6]-‘0’)*10 + (S[i+7]-‘0’);
+            calls_dur_hor[k] = (S[i]-'0')*10 + (S[i+1]-'0');
+            calls_dur_min[k] = (calls_dur_hor[k] * 60) + ((S[I+3]-'0')*10 + S[i+4]-'0'); 
+            calls_dur_seg[k] = (S[i+6]-'0')*10 + (S[i+7]-'0');
             //cout << calls_dur_min[k] << ":" << calls_dur_seg[k] << endl;
             k++;
         }
