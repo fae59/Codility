6248edce7784bfd1b474c467b76780860d6c8534
Frederico Gama <91661291+fae59@users.noreply.github.com>
2021-10-13 14:11:33 -0300

    Update testeSidia3.c

--- testeSidia3.c
+++ testeSidia3.c
@@ -5,13 +5,13 @@ int solution(string S, int k) {
 	//string S{"SMS messages are really short"};
     	// write your code in C++14 (g++ 6.2.0)
     	const int N = S.size();
-    	//const int k = 12;
-    	int cnt = 0;
+    	const int k = 12;
+    	//int cnt = 0;
     
-    	for (int i=0;i<N;i+=k) {
+    	//for (int i=0;i<N;i+=k) {
         	//cout << "split=" << S.substr(i, k) << endl;
-        	cnt++;
-    	}
-
-    return ceil((float)N/k);
+        	//cnt++;
+    	//}
+	
+	return ceil((float)N/k);
 }
